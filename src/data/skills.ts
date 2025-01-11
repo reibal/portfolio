d4287f61bc0369d713af8696d676ca5006fbe5cf
Kazz <i94773007@gmail.com>
2025-01-11 21:49:54 +0100

    feature: add technology icons and refactor skills data structure

---
+++ src/data/skills.ts
@@ -0,0 +1,80 @@
+import HtmlIcon from '../components/icons/technologies/HtmlIcon.astro'
+import GitIcon from '../components/icons/technologies/GitIcon.astro'
+import GithubIcon from '../components/icons/technologies/GithubIcon.astro'
+import ReactIcon from '../components/icons/technologies/ReactIcon.astro'
+import TailwindIcon from '../components/icons/technologies/TailwindIcon.astro'
+import AstroIcon from '../components/icons/technologies/AstroIcon.astro'
+import NextJSIcon from '../components/icons/technologies/NextJSIcon.astro'
+import TypeScriptIcon from '../components/icons/technologies/TypeScriptIcon.astro'
+import JavaScriptIcon from '../components/icons/technologies/JavaScriptIcon.astro'
+import CssIcon from '../components/icons/technologies/CssIcon.astro'
+import NodeJSIcon from '../components/icons/technologies/NodeJSIcon.astro'
+import DatabaseIcon from '../components/icons/technologies/DatabaseIcon.astro'
+import ExpressIcon from '../components/icons/technologies/ExpressIcon.astro'
+
+export default [
+	{
+		name: "HTML",
+		icon: HtmlIcon
+	},
+	{
+		name: "CSS",
+		icon: CssIcon
+	},
+	{
+		name: "JavaScript",
+		icon: JavaScriptIcon
+	},
+	{
+		name: "TypeScript",
+		icon: TypeScriptIcon
+	},
+	{
+		name: "React",
+		icon: ReactIcon
+	},
+	{
+		name: "NextJS",
+		icon: NextJSIcon
+	},
+	{
+		name: "Astro",
+		icon: AstroIcon
+	},
+	{
+		name: "Tailwind",
+		icon: TailwindIcon
+	},
+	{
+		name: "React Native",
+		icon: ReactIcon
+	},
+	{
+		name: "NodeJS",
+		icon: NodeJSIcon
+	},
+	{
+		name: "Express",
+		icon: ExpressIcon
+	},
+	{
+		name: "MySQL",
+		icon: DatabaseIcon
+	},
+	{
+		name: "PostgreSQL",
+		icon: DatabaseIcon
+	},
+	{
+		name: "MongoDB",
+		icon: DatabaseIcon
+	},
+	{
+		name: "Git",
+		icon: GitIcon
+	},
+	{
+		name: "GitHub",
+		icon: GithubIcon
+	}
+]
