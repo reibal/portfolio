44f87947e5673cd88a977eb1a01a4e60ac6c5170
Kazz <i94773007@gmail.com>
2025-02-24 00:39:23 +0100

    added new project entries for Study Decks and Link Shortener with associated images and technologies

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,13 +6,17 @@ import PrismaIcon from '../components/icons/technologies/PrismaIcon.astro'
 import DatabaseIcon from '../components/icons/technologies/DatabaseIcon.astro'
 import ReactIcon from '../components/icons/technologies/ReactIcon.astro'
 import AstroIcon from '../components/icons/technologies/AstroIcon.astro'
+import AngularIcon from '../components/icons/technologies/AngularIcon.astro'
+import CssIcon from '../components/icons/technologies/CssIcon.astro'
+import MongoDBIcon from '../components/icons/technologies/MongoDBIcon.astro'
+import ExpressIcon from '../components/icons/technologies/ExpressIcon.astro'
 
 // Projects Data
 export default [
 	{
 		title: {
 			es: "GlitchLink",
-			en: "GlitchLink",
+			en: "GlitchLink"
 		},
 		description: {
 			es: "Una red social para gamers, en la que tener tu lista de juegos favoritos y agregar a gente para jugar juntos.",
@@ -50,6 +54,64 @@ export default [
 		],
 		link: "https://glitchlink.vercel.app",
 		github: "https://github.com/KazzK94/glitchlink",
-		image: "/images/projects/glitchlink.webp",
+		image: "/images/projects/glitchlink.webp"
+	},
+	{
+		title: {
+			es: "Study Decks",
+			en: "Study Decks"
+		},
+		description: {
+			es: "Una aplicación muy sencilla para crear tus propias tarjetas de estudio y repasar de manera fácil y entretenida.",
+			en: "A very simple application to create your own study cards and review them in an easy and entertaining way."
+		},
+		technologies: [
+			{
+				name: "React",
+				icon: ReactIcon,
+			},
+			{
+				name: "Tailwind",
+				icon: TailwindIcon,
+			},
+			{
+				name: "Local Storage",
+				icon: DatabaseIcon
+			}
+		],
+		link: "https://cardstudy.netlify.app/",
+		github: "https://github.com/KazzK94/study-cards",
+		image: "/images/projects/studydecks.webp"
+	},
+	{
+		title: {
+			es: "Acortador de URLs (en proceso)",
+			en: "Link Shortener (WIP)"
+		},
+		description: {
+			es: "Un acortador de URLs muy simple. Este proyecto es el Front-End, la lógica para acortar la URL y guardar la información y las visitas está en el back-end indicado en el archivo README.md del repositorio de GitHub.",
+			en: "A URL shortener that is very simple. This project is the Front-End, the logic to shorten the URL and save the information and visits is in the back-end indicated in the README.md file in the GitHub repository."
+		},
+		technologies: [
+			{
+				name: "Angular 19",
+				icon: AngularIcon
+			},
+			{
+				name: "CSS",
+				icon: CssIcon
+			},
+			{
+				name: "Express",
+				icon: ExpressIcon
+			},
+			{
+				name: "MongoDB",
+				icon: MongoDBIcon
+			}
+		],
+		link: "https://shorturl.netlify.app/",
+		github: "https://github.com/KazzK94/url-shortener-client",
+		image: "/images/projects/urlshortener.webp"
 	}
 ]
