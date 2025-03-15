e8a185706ad63e77ba20bbe8c6a765864acdef17
Kazz <i94773007@gmail.com>
2025-03-15 05:55:10 +0100

    rolled back to basics for astro file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,18 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from "@astrojs/tailwind"
-
 import netlify from "@astrojs/netlify"
 
-// https://astro.build/config
 export default defineConfig({
 	site: "https://reibal.dev",
 	integrations: [tailwind()],
 	adapter: netlify(),
-	output: "server",
+	output: "static",
 	i18n: {
 		defaultLocale: "es",
 		locales: ["es", "en"],
 		routing: {
 			prefixDefaultLocale: false,
-		},
-		domains: {
-			"es": "https://reibal.dev",
-			"en": "https://en.reibal.dev"
 		}
 	},
 })
