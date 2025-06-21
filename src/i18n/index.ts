ed0306187ec88ffc524e020683041c4ad0f48ad7
Kazz <i94773007@gmail.com>
2025-06-21 15:55:12 +0200

    updated texts with current information

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,7 +2,18 @@
 import en from './en.json'
 import es from './es.json'
 
-export function getI18n({ currentLocale = 'es' }: { currentLocale: string | undefined }) {
-	if (currentLocale === 'en') return en
-	return es
+const locales = {
+	'en': en,
+	'es': es
+}
+type Locale = typeof locales
+
+const defaultLocaleKey = 'es'
+
+export function getI18n({ currentLocale }: { currentLocale: string | undefined }) {
+	// If currentLocale is a key of locales, return the corresponding locale
+	if (currentLocale && currentLocale in locales) {
+		return locales[currentLocale as keyof Locale]
+	}
+	return locales[defaultLocaleKey]
 }
